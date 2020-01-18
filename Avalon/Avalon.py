49bb0ffe0e67b5d1e76a814d540016da68c536a9
mdk44 <matt.karam@gmail.com>
2020-01-17 20:13:25 -0600

    Tinkering

--- Avalon/Avalon.py
+++ Avalon/Avalon.py
@@ -3,17 +3,17 @@
 import random
 
 character_list = {
-    0: 'Merlin',
-    1: 'Percival',
+    0: 'Merlin', # Knows all evil except Mordred
+    1: 'Percival', # Knows who Merlin is
     2: 'Loyal Servant of Arthur',
     3: 'Loyal Servant of Arthur',
     4: 'Loyal Servant of Arthur',
     5: 'Loyal Servant of Arthur',
     6: 'Loyal Servant of Arthur',
-    7: 'Mordred',
-    8: 'Assassin',
-    9: 'Oberon',
-    10: 'Morgana',
+    7: 'Mordred', # Unknown to Merlin
+    8: 'Assassin', # Gets to attempt assassination of Merlin if good guys 'win'
+    9: 'Oberon', # Unknown to evil
+    10: 'Morgana', # Appears as Merlin to Percival
     11: 'Minion of Mordred',
     12: 'Minion of Mordred',
     13: 'Minion of Mordred'
