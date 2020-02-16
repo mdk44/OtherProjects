ccb725399d136dd95cabf142897b6ab6e822db81
mdk44 <matt.karam@gmail.com>
2020-02-16 09:04:19 -0600

    Testing functionality

---
+++ Send Email/Email.py
@@ -0,0 +1,15 @@
+import smtplib
+
+# Establish a secure session with gmail's outgoing SMTP server using your gmail account
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.ehlo()
+server.starttls()
+server.login('email address', 'password')
+server.sendmail('from email address', 'to email address', 'Subject: Test \
+    \nTest email.')
+server.quit()
+
+# Send text message through SMS gateway of destination number
+# server.sendmail('matt.karam@gmail.com', '2104626492@messaging.sprintpcs.com', 'Hello') # Text message
+
+# Need to enable less secure apps to access your Google account for this to work
